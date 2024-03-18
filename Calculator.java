969d1842b9bbd660c034d04783de10641064782f
KBodko <163579494+KBodko@users.noreply.github.com>
2024-03-18 18:40:15 +0100

    Add files via upload
    version two

--- Calculator.java
+++ Calculator.java
@@ -1,53 +1,78 @@
-
 import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Vitaj v aplikácií Aj Ty v IT Kalkulačka :)");
 
-        Scanner input = new Scanner(System.in);
-        Scanner userChoice = new Scanner(System.in);
-        String answer;
-        int firstNumber;
-        int secondNumber;
+        do {
+            String answer = loadUserChoice();
+            Scanner userInputNumbers = new Scanner(System.in);
+            System.out.println("Zadaj prvé číslo: ");
+            int firstNumber = userInputNumbers.nextInt();
+            System.out.println("Zadaj druhé číslo: ");
+            int secondNumber = userInputNumbers.nextInt();
+            mathOperations(answer, firstNumber, secondNumber);
 
-        for (int i= 1; i>0; i++) {
-
-                while (true) {
-                    calculator();
-                    answer = input.nextLine();
-                    if (isAnswerValid(answer)) {
-                        break;
-                    }
-                    System.out.println("Takáto možnosť nie je. Skús to prosím znova.");
-                }
-
-                System.out.println("Zadaj prvé číslo:");
-                firstNumber = input.nextInt();
-                System.out.println("Zadaj druhé číslo");
-                secondNumber = input.nextInt();
-
-                mathOperations(answer, firstNumber, secondNumber);
-
-
-                while (true) {
-                    countAgain();
-                    answer = userChoice.nextLine();
-                    if (nextValidation(answer)) {
-                        break;
-                    }
-                    System.out.println("Takáto možnosť nie je. Skús to prosím znova.");
-                }
-                if (answer.equals("nie")) {
-                    System.out.println("Tento program sa teraz ukončí.");
-                    break;
-                }
-
-        }
+        } while (wantToCountAgain());
 
     }
 
-    public static void mathOperations (String answer, int firstNumber, int secondNumber){
+    public static boolean isChoiceValid(String answer) {
+        return answer.equals("a") || answer.equals("b") || answer.equals("c") || answer.equals("d");
+    }
+    public static String  loadUserChoice (){
+        System.out.println("Zvoľ akú operáciu chceš vykonať:");
+        System.out.println("a) sčítanie");
+        System.out.println("b) odčítanie");
+        System.out.println("c) násobenie");
+        System.out.println("d) delenie");
+        System.out.println("Zadaj svoju voľbu: ");
+        Scanner userChoice = new Scanner(System.in);
+        String answer;
+            do {
+                answer = userChoice.nextLine();
+                if (isChoiceValid(answer)) {
+                    return answer;
+                }
+                System.out.println("Takáto možnosť nie je. Skús to ešte raz.");
+            } while (true);
+
+
+
+    }
+    public  static boolean wantToContinueValidation(String answer) {
+        return answer.equals("a") || answer.equals("b");
+    }
+    public static String loadUserAnswer (){
+        Scanner userInput = new Scanner(System.in);
+        String answer;
+            do {
+                answer = userInput.nextLine();
+                if (wantToContinueValidation (answer)) {
+                    return answer;
+                }
+                System.out.println("Takáto možnosť nie je skús to prosím znova.");
+            }while (true);
+
+    }
+    public static boolean wantToCountAgain () {
+        System.out.println("Chceš pokračovať v počítaní? ");
+        System.out.println("a) áno");
+        System.out.println("b) nie");
+        System.out.println("Zadaj svoju voľbu: ");
+        String answer = loadUserAnswer();
+            if (answer.equals("b")) {
+                System.out.println("Aplikácia kalkulačka sa teraz ukončí.");
+                return false;
+            }
+        return true;
+    }
+
+    public static double division(int number1, int number2) {
+        return (double) number1 / (double) number2;
+
+    }
+    public static void mathOperations (String answer, int firstNumber, int secondNumber) {
         switch (answer) {
             case "a":
                 System.out.println("Výsledok sčítania je : " + Integer.sum(firstNumber, secondNumber));
@@ -60,34 +85,14 @@ public class Calculator {
                 break;
             default:
                 System.out.println("Výsledok delenia je : " + division(firstNumber, secondNumber));
-
         }
-
-    }
-    public static boolean nextValidation (String answer){
-        return answer.equals("áno") || answer.equals("nie");
-    }
-    public static void countAgain (){
-        System.out.println("Chceš pokračovať v počítaní? ");
-        System.out.println("áno");
-        System.out.println("nie");
-        System.out.println("Zadaj svoju voľbu: ");
-
-    }
-    public static double division(int number1, int number2) {
-        return (double) number1 / (double) number2;
-
-    }
-    public static boolean isAnswerValid(String answer) {
-        return answer.equals("a") || answer.equals("b") || answer.equals("c") || answer.equals("d");
     }
 
-    public static void calculator() {
-        System.out.println("Zvoľ akú operáciu chceš vykonať:");
-        System.out.println("a) sčítanie");
-        System.out.println("b) odčítanie");
-        System.out.println("c) násobenie");
-        System.out.println("d) delenie");
-        System.out.println("Zadaj svoju voľbu: ");
-    }
 }
+
+
+
+
+
+
+
